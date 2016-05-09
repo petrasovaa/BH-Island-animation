05e38fa3c1aeff70f6e6a8179d1bc3575debfbbf
Anna Petrasova <kratochanna@gmail.com>
2016-05-08 23:03:37 -0400

    add coastlines and dashed 2014 coastline

--- scripts/island.sh
+++ scripts/island.sh
@@ -6,6 +6,20 @@ export GRASS_RENDER_FILE_READ=TRUE
 export GRASS_FONT=/usr/share/fonts/truetype/freefont/FreeSansBold.ttf
 
 g.region raster=elevation_1997 res=2 -a
+
+# create dashed line
+D=50
+DD=$(($D + $D))
+S=0
+rm seg_tmp.txt
+for I in `seq 0 $DD 10500`
+do
+   D_TMP=$(($D))
+   echo "L 1 1 $S $(($S + $D))" >> seg_tmp.txt
+   S=$(($S + $DD))
+done
+v.segment --o input=shoreline_2014_cat output=shoreline_2014_seg rules=seg_tmp.txt
+
 rm *.png
 rm *.gif
 #for E in elevation_1997 #`g.list type=raster mapset=elevation_timeseries exc="*ground"`
@@ -14,24 +28,16 @@ for E in `g.list type=raster pattern="coast*"` `g.list type=raster mapset=elevat
 
 do
     r.mapcalc "new = if($E > 0.01, 1, 0)" --o --q
-    r.mapcalc "new2 = if (isnull(inner), if(new == 1, 1, null()), 1)" --o --q
-    r.to.vect new2 type=area output=new3 -s --o --q
-    v.clean input=new3 output=new4 tool=rmarea thres=10000 -c --o --q
-    if [[ "$E" =~ coast* ]]; then
-        g.rename vector=new3,new5 --o 
-    else
-        echo $E
-        v.generalize input=new4 output=new5 method=reumann thre=5 --o --q
-        v.generalize input=new5 output=$E method=boyle thre=400 --o --q
-    fi
     r.colors new --q rules=- <<EOF
-0 195:245:255
+0 130:220:235
 1 239:198:42
 EOF
     T=`r.timestamp map=$E`
     T="`python -c """print '$T'.split()[2] if len('$T'.split()) < 7 else '$T'.split()[2] + ' - ' + '$T'.split()[6]"""`"
+    ST="`python -c """print '$T'.replace(' - ', '_') if '-' in '$T' else '$T'"""`"
     d.rast new
-    d.vect $E fcolor=none color=98:85:36 width=3 --q
+    d.vect map=shoreline_${ST} color=98:85:36 width=3 --q
+    d.vect map=shoreline_2014_seg color=98:85:36 width=3 --q
     d.vect map=transportation@PERMANENT where="highway = 'path'" width=2 color=50:50:50 --q
     d.vect map=transportation@PERMANENT where="highway <> 'path'" color=94:94:94 width=1 --q
     d.vect map=buildings color=none fill_color=77:77:77 width=1 --q
@@ -42,7 +48,7 @@ EOF
     optipng -o5 $E-nq8.png
     mv $E-nq8.png $E.png
 done
-convert -delay 1x1  `ls elevation* -tr` -coalesce -layers OptimizeTransparency animation.gif
+convert -delay 1x1  `ls coast* elevation* -tr` -coalesce -layers OptimizeTransparency animation.gif
 convert animation.gif \( +clone -set delay 500 \) +swap +delete  animation_with_pause.gif
 
 for I in `ls *.png -tr`
